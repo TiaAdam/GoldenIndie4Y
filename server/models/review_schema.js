167ab51353c8f8f5585b44259812e92e6ea001d7
TiaAdam <44812614+TiaAdam@users.noreply.github.com>
2021-05-19 13:45:06 +0200

    Add files via upload

--- server/models/review_schema.js
+++ server/models/review_schema.js
@@ -2,6 +2,15 @@ const { Schema, model } = require('mongoose');
 
 const reviewSchema = new Schema(
   {
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'users',
+      required: [true, 'User ID field is required!'],
+    },
+    title: {
+      type: String,
+      required: [true, 'Title field is required!'],
+    },
     content: {
       type: String,
       required: [true, 'content field is required'],
