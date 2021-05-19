a0fd9248cf04965a442fa0d6da640072d649ba8c
TiaAdam <44812614+TiaAdam@users.noreply.github.com>
2021-05-19 13:46:05 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+// Importing required modules
+const cors = require('cors');
+const express = require('express');
+
+// parse env variables
+require('dotenv').config();
+
+require("./helpers/db/mongodb.js")();
+
+// Configuring port
+const port = process.env.PORT || 9000;
+
+const app = express();
+
+// Configure middlewares
+app.use(cors());
+app.use(express.json());
+
+app.set('view engine', 'html');
+
+// Static folder
+app.use(express.static(__dirname + '/views/'));
+
+// Defining route middleware
+app.use('/api/authors', require('./routes/user_routes'));
+app.use('/api/reviews', require('./routes/review_routes'));
+app.use('/api/articles', require('./routes/article_routes'));
+app.use('/api/roles', require('./routes/role_routes'));
+
+
+// Listening to port
+app.listen(port);
+console.log(`Listening On http://localhost:${port}/api`);
+
+module.exports = app;
